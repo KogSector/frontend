c50dfef723468a6ed19ffcf65b093813e9c75db7
Rishabh Das <rishabh.babi@gmail.com>
2026-06-10 02:09:08 +0530

    .

--- src/components/icons/GoogleDriveIcon.tsx
+++ src/components/icons/GoogleDriveIcon.tsx
@@ -5,9 +5,9 @@ import React from 'react'
 export default function GoogleDriveIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path fill="#4285F4" d="M8.5 3l3.5 6-5.5 9H0l8.5-15z"/>
-      <path fill="#34A853" d="M24 18l-3.5 6H6.5l4.5-6H24z"/>
-      <path fill="#FBBC05" d="M15.5 3L24 18h-9l-3.5-6L12 9l3.5-6z"/>
+      <path fill="#FFC107" d="M8.4 20h7.2l8.4-14.4h-7.2L8.4 20z" />
+      <path fill="#1976D2" d="M0 14.4l3.6 6 8.4-14.4-3.6-6L0 14.4z" />
+      <path fill="#4CAF50" d="M8.4 20l-3.6-6H19.2l3.6 6H8.4z" />
     </svg>
   )
 }
