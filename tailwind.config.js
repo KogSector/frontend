0882dc3ae68e5a0d3fa20e3c528b1ee71cf409df
Rishabh Das <rishabh.babi@gmail.com>
2026-06-27 13:56:10 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
+        "primary-glow": "hsl(var(--primary-glow))",
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
