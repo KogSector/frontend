54daa3e68808c55189cee410ea5b7e728914015f
Rishabh Das <rishabh.babi@gmail.com>
2026-06-28 00:21:06 +0530

    .

--- test.js
+++
@@ -1,3 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: 'postgresql://neondb_owner:npg_PfJsZg49bUxT@ep-small-voice-a1o0n6xl-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require' });
-pool.query("SELECT table_schema, table_name FROM information_schema.tables WHERE table_schema NOT IN ('information_schema', 'pg_catalog')").then(res => { console.log(res.rows); pool.end(); });
